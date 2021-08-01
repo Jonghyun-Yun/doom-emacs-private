7511a1230681e43e73ff97decd487c090cd51c73
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-07-31 19:35:26 -0500

    bump: langtool-ignore-fonts

--- modules/private/grammar/packages.el
+++ modules/private/grammar/packages.el
@@ -4,5 +4,6 @@
 (package! langtool :pin "8276eccc5587bc12fd205ee58a7a982f0a136e41")
 (package! writegood-mode :pin "b71757ec337e226909fb0422f0224e31acc71733")
 (package! langtool-ignore-fonts
-  :recipe (:host github :repo "cjl8zf/langtool-ignore-fonts")
-  :pin "91428f59915380c5d2866a587a0023eb0cde7d2e")
+  ;; :recipe (:host github :repo "cjl8zf/langtool-ignore-fonts")
+  ;; :pin "91428f59915380c5d2866a587a0023eb0cde7d2e"
+  :pin "c3291c85b733b9047653cbb1f525655394610bdb")
