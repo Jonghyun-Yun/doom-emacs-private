33edecf0c8b58b63ff6e384eafbc6eee8da51e70
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-09-20 07:40:06 -0500

    theme

--- config.el
+++ config.el
@@ -64,8 +64,11 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one-light)
+;; (setq doom-theme 'modus-operandi-theme)
 ;; (setq doom-theme 'doom-nord)
 ;; (load-theme 'modus-operandi-theme t)
+(remove-hook 'window-setup-hook #'doom-init-theme-h)
+(add-hook 'after-init-hook #'doom-init-theme-h 'append)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
