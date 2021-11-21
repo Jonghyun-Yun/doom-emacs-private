ea5d7756cbd4b1c4437fa5b074d1eabb13fcd594
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-11-20 19:10:45 -0600

    latex fold

--- local/latex-plus.el
+++ local/latex-plus.el
@@ -32,35 +32,6 @@
 
   )
 
-;; (with-eval-after-load 'company-auctex
-;;   (defun company-auctex-labels (command &optional arg &rest ignored)
-;;     "company-auctex-labels backend"
-;;     (interactive (list 'interactive))
-;;     (case command
-;;       (interactive (company-begin-backend 'company-auctex-labels))
-;;       (prefix (company-auctex-prefix "\\\\?ref{\\([^}]*\\)\\="))
-;;       (candidates (company-auctex-label-candidates arg))))
-
-;;   (defun company-auctex-bibs (command &optional arg &rest ignored)
-;;     "company-auctex-bibs backend"
-;;     (interactive (list 'interactive))
-;;     (case command
-;;       (interactive (company-begin-backend 'company-auctex-bibs))
-;;       (prefix (company-auctex-prefix "\\\\cite?[^[{]*\\(?:\\[[^]]*\\]\\)?{\\([^},]*\\)\\="))
-;;       (LaTeX-add-all-bibitems-from-bibtex)
-;;       (candidates (company-auctex-bib-candidates arg))))
-;;   )
-
-;; (defun get-bibtex-keys (file)
-;;   (with-current-buffer (find-file-noselect file)
-;;     (mapcar 'car (bibtex-parse-keys))))
-
-;; (defun LaTeX-add-all-bibitems-from-bibtex ()
-;;   (interactive)
-;;   (mapc 'LaTeX-add-bibitems
-;;         (apply 'append
-;;                (mapcar 'get-bibtex-keys (reftex-get-bibfile-list)))))
-
 ;;; reftex
 ;; Make RefTeX faster
 (with-eval-after-load 'reftex
@@ -98,7 +69,6 @@
  )
 
 ;; ;; RefTeX bindings
-;; (setq reftex-refstyle "\\cref")
 ;; (eval-after-load "reftex"
 ;;   '(progn
 ;;      (define-key reftex-mode-map (kbd "C-c )")
@@ -219,7 +189,13 @@
   ;;; tex-fold
   (setcar (assoc "⋆" LaTeX-fold-math-spec-list) "★") ;; make \star bigger
   (setq TeX-fold-math-spec-list
-        `(
+        `(;; missing/better symbols
+          ("≤" ("le"))
+          ("≥" ("ge"))
+          ("≠" ("ne"))
+          ;; convenience shorts -- these don't work nicely ATM
+          ;; ("‹" ("left"))
+          ;; ("›" ("right"))
           ;; private macros
           ("ℝ" ("RR"))
           ("ℕ" ("NN"))
@@ -231,40 +207,39 @@
           ("𝔼" ("EE"))
           ("𝑑" ("dd"))
           ;; mathbb
-          ("ℝ" ("mathbb{R}"))
-          ("ℕ" ("mathbb{N}"))
-          ("ℤ" ("mathbb{Z}"))
-          ("ℚ" ("mathbb{Q}"))
-          ("ℂ" ("mathbb{C}"))
-          ("ℙ" ("mathbb{P}"))
-          ("ℍ" ("mathbb{H}"))
-          ("𝔼" ("mathbb{E}"))
-          ("𝑑" ("mathbb{d}"))
-
+          ;; ("ℝ" ("mathbb{R}"))
+          ;; ("ℕ" ("mathbb{N}"))
+          ;; ("ℤ" ("mathbb{Z}"))
+          ;; ("ℚ" ("mathbb{Q}"))
+          ;; ("ℂ" ("mathbb{C}"))
+          ;; ("ℙ" ("mathbb{P}"))
+          ;; ("ℍ" ("mathbb{H}"))
+          ;; ("𝔼" ("mathbb{E}"))
+          ;; ("𝑑" ("mathbb{d}"))
           ;; known commands
-          ;; ("" ("phantom"))
+          ("" ("phantom"))
           ;; (,(lambda (num den) (if (and (TeX-string-single-token-p num) (TeX-string-single-token-p den))
-          ;;                        (concat num "／" den)
-          ;;                      (concat "❪" num "／" den "❫"))) ("frac"))
+          ;;                    (concat num "／" den)
+          ;;                  (concat "❪" num "／" den "❫"))) ("frac"))
           ;; (,(lambda (arg) (concat "√" (TeX-fold-parenthesize-as-necessary arg))) ("sqrt"))
           ;; (,(lambda (arg) (concat "⭡" (TeX-fold-parenthesize-as-necessary arg))) ("vec"))
-          ;; ("‘{1}’" ("text"))
+          ("‘{1}’" ("text"))
           ;; private commands
           ("|{1}|" ("abs"))
           ("‖{1}‖" ("norm"))
           ("⌊{1}⌋" ("floor"))
           ("⌈{1}⌉" ("ceil"))
-          ;; ("⌊{1}⌉" ("round"))
+          ("⌊{1}⌉" ("round"))
           ("𝑑{1}/𝑑{2}" ("dv"))
           ("∂{1}/∂{2}" ("pdv"))
           ;; fancification
-          ;; ("{1}" ("mathrm"))
-          ;; (,(lambda (word) (string-offset-roman-chars 119743 word)) ("mathbf"))
-          ;; (,(lambda (word) (string-offset-roman-chars 119951 word)) ("mathcal"))
-          ;; (,(lambda (word) (string-offset-roman-chars 120003 word)) ("mathfrak"))
-          ;; (,(lambda (word) (string-offset-roman-chars 120055 word)) ("mathbb"))
-          ;; (,(lambda (word) (string-offset-roman-chars 120159 word)) ("mathsf"))
-          ;; (,(lambda (word) (string-offset-roman-chars 120367 word)) ("mathtt"))
+          ("{1}" ("mathrm"))
+          (,(lambda (word) (string-offset-roman-chars 119743 word)) ("mathbf"))
+          (,(lambda (word) (string-offset-roman-chars 119951 word)) ("mathcal"))
+          (,(lambda (word) (string-offset-roman-chars 120003 word)) ("mathfrak"))
+          (,(lambda (word) (string-offset-roman-chars 120055 word)) ("mathbb"))
+          (,(lambda (word) (string-offset-roman-chars 120159 word)) ("mathsf"))
+          (,(lambda (word) (string-offset-roman-chars 120367 word)) ("mathtt"))
           )
         TeX-fold-macro-spec-list
         '(
@@ -275,6 +250,8 @@
           ("[r]" ("ref" "pageref" "eqref"))
           ("[i]" ("index" "glossary"))
           ("..." ("dots"))
+          ("{1}" ("emph" "textit" "textsl" "textmd" "textrm" "textsf" "texttt"
+                  "textbf" "textsc" "textup"))
           ;; tweaked defaults
           ("©" ("copyright"))
           ("®" ("textregistered"))
@@ -287,26 +264,61 @@
           ("§§§ {1}" ("subsubsection" "subsubsection*"))
           ("¶ {1}" ("paragraph" "paragraph*"))
           ("¶¶ {1}" ("subparagraph" "subparagraph*"))
-          )))
+          ;; extra
+          ;; ("⬖ {1}" ("begin"))
+          ;; ("⬗ {1}" ("end"))
+          ))
 
+  (defun string-offset-roman-chars (offset word)
+    "Shift the codepoint of each character in WORD by OFFSET with an extra -6 shift if the letter is lowercase"
+    (apply 'string
+           (mapcar (lambda (c)
+                     (string-offset-apply-roman-char-exceptions
+                      (+ (if (>= c 97) (- c 6) c) offset)))
+                   word)))
 
-;; Making \( \) less visible
-;; (defface unimportant-latex-face
-;;   '((t :inherit font-lock-comment-face :weight extra-light))
-;;   "Face used to make \\(\\), \\[\\] less visible."
-;;   :group 'LaTeX-math)
+  (defvar string-offset-roman-char-exceptions
+    '(;; lowercase serif
+      (119892 .  8462) ; ℎ
+      ;; lowercase caligraphic
+      (119994 . 8495) ; ℯ
+      (119996 . 8458) ; ℊ
+      (120004 . 8500) ; ℴ
+      ;; caligraphic
+      (119965 . 8492) ; ℬ
+      (119968 . 8496) ; ℰ
+      (119969 . 8497) ; ℱ
+      (119971 . 8459) ; ℋ
+      (119972 . 8464) ; ℐ
+      (119975 . 8466) ; ℒ
+      (119976 . 8499) ; ℳ
+      (119981 . 8475) ; ℛ
+      ;; fraktur
+      (120070 . 8493) ; ℭ
+      (120075 . 8460) ; ℌ
+      (120076 . 8465) ; ℑ
+      (120085 . 8476) ; ℜ
+      (120092 . 8488) ; ℨ
+      ;; blackboard
+      (120122 . 8450) ; ℂ
+      (120127 . 8461) ; ℍ
+      (120133 . 8469) ; ℕ
+      (120135 . 8473) ; ℙ
+      (120136 . 8474) ; ℚ
+      (120137 . 8477) ; ℝ
+      (120145 . 8484) ; ℤ
+      )
+    "An alist of deceptive codepoints, and then where the glyph actually resides.")
 
-;; (font-lock-add-keywords
-;;  'latex-mode
-;;  `((,(rx (and "\\" (any "()[]"))) 0 'unimportant-latex-face prepend))
-;;  'end)
-
-;; (font-lock-add-keywords
-;;  'latex-mode
-;;  `((,"\\\\[[:word:]]+" 0 'font-lock-keyword-face prepend))
-;;  'end)
+  (defun string-offset-apply-roman-char-exceptions (char)
+    "Sometimes the codepoint doesn't contain the char you expect.
+Such special cases should be remapped to another value, as given in `string-offset-roman-char-exceptions'."
+    (if (assoc char string-offset-roman-char-exceptions)
+        (cdr (assoc char string-offset-roman-char-exceptions))
+      char))
+  )
 
 ;;; orgtbl mode
 ;; https://github.com/karthink/lazytab
 ;; (after! latex
-  ;; (load! "~/.doom.d/local/lazytab/lazytab.el"))
+;; (load! "~/.doom.d/local/lazytab/lazytab.el"))
