91308452f54ba13dda3ce7d0136c172193663510
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-11-26 13:30:25 -0600

    move to overleaf package

--- autoload/vc.el
+++ autoload/vc.el
@@ -1,46 +1,6 @@
 ;;; ~/.doom.d/autoload/vc.el -*- lexical-binding: t; -*-
 ;;;###if (featurep! :emacs vc)
 
-;;;###autoload
-(defun jyun/setup-overleaf-pull ()
-"Add hook for local overleaf pull."
-  (progn
-    (setq-local overleaf-directory (file-truename (ffip-project-root)))
-    (add-hook 'projectile-after-switch-project-hook (lambda () (jyun/magit-pull-overleaf overleaf-directory))))
-  )
-
-;;;###autoload
-(defun jyun/setup-overleaf-push ()
-"Add hook for local overleaf push."
-  (progn
-    (setq-local overleaf-directory (file-truename (ffip-project-root)))
-    (add-hook 'after-save-hook (lambda () (jyun/magit-push-overleaf overleaf-directory)))
-    ))
-
-;;;###autoload
-(defun jyun/magit-push-overleaf (directory)
-  "Use Magit to stage files if there are unstaged ones.
-Call asynchronous magit processes to commit and push staged files (if exist) to origin"
-(when directory
-      (unless (featurep 'magit) (require 'magit))
-      (let ((default-directory (magit-toplevel directory)))
-        (when (or (magit-anything-unstaged-p) (magit-anything-staged-p))
-          (magit-with-toplevel
-            (magit-stage-1 "--u" magit-buffer-diff-files))
-          (let ((message (format "pushing changes %s" (format-time-string "%Y-%m-%d %H:%M:%S %Z"))))
-            (magit-run-git-async "commit" "-m" message)
-            (magit-run-git-async "push" "origin" "master")
-            )))))
-
-;;;###autoload
-(defun jyun/magit-pull-overleaf (directory)
-  "Run `git pull origin master' using asynchronous magit processes."
-  (when directory
-    (unless (featurep 'magit) (require 'magit))
-    (let ((default-directory (magit-toplevel directory)))
-    (magit-run-git-async "pull" "origin" "master"))
-  ))
-
 ;; Ediff the init.example.el and my init.el
 ;;;###autoload
 (defun ediff-init-files ()
