86826c305ca35af4add208118e499e18b2788034
Jonghyun Yun <jonghyun.yun@gmail.com>
2021-12-04 10:30:42 -0600

    ebook

--- modules/private/reading/packages.el
+++ modules/private/reading/packages.el
@@ -4,8 +4,5 @@
 ;; speed reading
 (package! spray :pin "66fa9daa53afd76553a2a8ffa212e70c5350dbd8" :disable t)
 
-;; epub
-(package! nov :disable t)
-
 ;; Emacs info
 (package! info-colors :pin "47ee73cc19b1049eef32c9f3e264ea7ef2aaf8a5")
