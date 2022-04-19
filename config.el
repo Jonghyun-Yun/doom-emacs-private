ad60c76ccee03134010baa6275470579fe51418c
Jonghyun Yun <jonghyun.yun@gmail.com>
2022-04-18 20:10:53 -0500

    fix missing paran

--- config.el
+++ config.el
@@ -1722,127 +1722,114 @@ capture was not aborted."
  "⭠ now ─────────────────────────────────────────────────")
 
 (add-hook 'org-mode-hook #'org-modern-mode)
-(add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+(add-hook 'org-agenda-finalize-hook #'org-modern-agenda))
 
 ;;; appearance
 (use-package! lin
-:config
-(setq lin-face 'lin-blue) ; check doc string for alternative styles
+  :config
+  (setq lin-face 'lin-blue) ; check doc string for alternative styles
 
-;; You can use this to live update the face:
-;;
-;; (customize-set-variable 'lin-face 'lin-green)
+  ;; You can use this to live update the face:
+  ;;
+  ;; (customize-set-variable 'lin-face 'lin-green)
 
-(setq lin-mode-hooks
-      '(bongo-mode-hook
-        dired-mode-hook
-        elfeed-search-mode-hook
-        git-rebase-mode-hook
-        grep-mode-hook
-        ibuffer-mode-hook
-        ilist-mode-hook
-        ledger-report-mode-hook
-        log-view-mode-hook
-        magit-log-mode-hook
-        mu4e-headers-mode
-        notmuch-search-mode-hook
-        notmuch-tree-mode-hook
-        occur-mode-hook
-        org-agenda-mode-hook
-        proced-mode-hook
-        tabulated-list-mode-hook))
+  (setq lin-mode-hooks
+        '(bongo-mode-hook
+          dired-mode-hook
+          elfeed-search-mode-hook
+          git-rebase-mode-hook
+          grep-mode-hook
+          ibuffer-mode-hook
+          ilist-mode-hook
+          ledger-report-mode-hook
+          log-view-mode-hook
+          magit-log-mode-hook
+          mu4e-headers-mode
+          notmuch-search-mode-hook
+          notmuch-tree-mode-hook
+          occur-mode-hook
+          org-agenda-mode-hook
+          proced-mode-hook
+          tabulated-list-mode-hook))
 
-(lin-global-mode 1)
+  (lin-global-mode 1)
   )
 
 (use-package! pulsar
-:config
-(setq pulsar-pulse-functions
-      ;; NOTE 2022-04-09: The commented out functions are from before
-      ;; the introduction of `pulsar-pulse-on-window-change'.  Try that
-      ;; instead.
-      '(recenter-top-bottom
-        move-to-window-line-top-bottom
-        reposition-window
-        ;; bookmark-jump
-        ;; other-window
-        ;; delete-window
-        ;; delete-other-windows
-        forward-page
-        backward-page
-        scroll-up-command
-        scroll-down-command
-        ;; windmove-right
-        ;; windmove-left
-        ;; windmove-up
-        ;; windmove-down
-        ;; windmove-swap-states-right
-        ;; windmove-swap-states-left
-        ;; windmove-swap-states-up
-        ;; windmove-swap-states-down
-        ;; tab-new
-        ;; tab-close
-        ;; tab-next
-        org-next-visible-heading
-        org-previous-visible-heading
-        org-forward-heading-same-level
-        org-backward-heading-same-level
-        outline-backward-same-level
-        outline-forward-same-level
-        outline-next-visible-heading
-        outline-previous-visible-heading
-        outline-up-heading))
+  :config
+  (setq pulsar-pulse-functions
+        ;; NOTE 2022-04-09: The commented out functions are from before
+        ;; the introduction of `pulsar-pulse-on-window-change'.  Try that
+        ;; instead.
+        '(recenter-top-bottom
+          move-to-window-line-top-bottom
+          reposition-window
+          ;; bookmark-jump
+          ;; other-window
+          ;; delete-window
+          ;; delete-other-windows
+          forward-page
+          backward-page
+          scroll-up-command
+          scroll-down-command
+          ;; windmove-right
+          ;; windmove-left
+          ;; windmove-up
+          ;; windmove-down
+          ;; windmove-swap-states-right
+          ;; windmove-swap-states-left
+          ;; windmove-swap-states-up
+          ;; windmove-swap-states-down
+          ;; tab-new
+          ;; tab-close
+          ;; tab-next
+          org-next-visible-heading
+          org-previous-visible-heading
+          org-forward-heading-same-level
+          org-backward-heading-same-level
+          outline-backward-same-level
+          outline-forward-same-level
+          outline-next-visible-heading
+          outline-previous-visible-heading
+          outline-up-heading))
 
-(setq pulsar-pulse-on-window-change t)
-(setq pulsar-pulse t)
-(setq pulsar-delay 0.055)
-(setq pulsar-iterations 10)
-(setq pulsar-face 'pulsar-magenta)
-(setq pulsar-highlight-face 'pulsar-yellow)
+  (setq pulsar-pulse-on-window-change t)
+  (setq pulsar-pulse t)
+  (setq pulsar-delay 0.055)
+  (setq pulsar-iterations 10)
+  (setq pulsar-face 'pulsar-magenta)
+  (setq pulsar-highlight-face 'pulsar-yellow)
 
-(pulsar-global-mode 1)
+  (pulsar-global-mode 1)
 
-;; OR use the local mode for select mode hooks
+  ;; OR use the local mode for select mode hooks
 
-(dolist (hook '(org-mode-hook emacs-lisp-mode-hook))
-  (add-hook hook #'pulsar-mode))
+  (dolist (hook '(org-mode-hook emacs-lisp-mode-hook))
+    (add-hook hook #'pulsar-mode))
 
-;; pulsar does not define any key bindings.  This is just a sample that
-;; respects the key binding conventions.  Evaluate:
-;;
-;;     (info "(elisp) Key Binding Conventions")
-;;
-;; The author uses C-x l for `pulsar-pulse-line' and C-x L for
-;; `pulsar-highlight-line'.
-;;
-;; You can replace `pulsar-highlight-line' with the command
-;; `pulsar-highlight-dwim'.
-(let ((map global-map))
-  (define-key map (kbd "C-c h p") #'pulsar-pulse-line)
-  (define-key map (kbd "C-c h h") #'pulsar-highlight-line))
-;; integration with the `consult' package:
-(add-hook 'consult-after-jump-hook #'pulsar-recenter-top)
-(add-hook 'consult-after-jump-hook #'pulsar-reveal-entry)
+  ;; pulsar does not define any key bindings.  This is just a sample that
+  ;; respects the key binding conventions.  Evaluate:
+  ;;
+  ;;     (info "(elisp) Key Binding Conventions")
+  ;;
+  ;; The author uses C-x l for `pulsar-pulse-line' and C-x L for
+  ;; `pulsar-highlight-line'.
+  ;;
+  ;; You can replace `pulsar-highlight-line' with the command
+  ;; `pulsar-highlight-dwim'.
+  (let ((map global-map))
+    (define-key map (kbd "C-c h p") #'pulsar-pulse-line)
+    (define-key map (kbd "C-c h h") #'pulsar-highlight-line))
+  ;; integration with the `consult' package:
+  (add-hook 'consult-after-jump-hook #'pulsar-recenter-top)
+  (add-hook 'consult-after-jump-hook #'pulsar-reveal-entry)
 
-;; integration with the built-in `imenu':
-(add-hook 'imenu-after-jump-hook #'pulsar-recenter-top)
-(add-hook 'imenu-after-jump-hook #'pulsar-reveal-entry)
+  ;; integration with the built-in `imenu':
+  (add-hook 'imenu-after-jump-hook #'pulsar-recenter-top)
+  (add-hook 'imenu-after-jump-hook #'pulsar-reveal-entry)
   )
 
 ;;; window split
-(defun jyun/window-split-grid ()
-  (interactive)
-  (delete-other-windows)
-  (evil-window-vsplit)
-  (evil-window-vsplit)
-  (evil-window-split)
-  (winum-select-window-2)
-  (evil-window-split)
-  (winum-select-window-1)
-  (evil-window-split)
-  )
-
-;; Window Split
 (defun spacemacs--window-split-splittable-windows ()
   (seq-remove
    (lambda (window)
