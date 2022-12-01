d743c355b8a5456b9b61f584123399dc1ddd7a23
Jonghyun Yun <jonghyun.yun@gmail.com>
2022-12-01 14:58:28 -0600

    citar pdf open

--- autoload/bibtex.el
+++ autoload/bibtex.el
@@ -15,19 +15,35 @@
 
 ;;; citar
 ;;;###autoload
-(defun citar-open-bibtex-pdf (keys-entries)
-  "Open pdf file associated with the KEYS-ENTRIES.
+;; (defun citar-open-bibtex-pdf (keys-entries)
+;;   "Open pdf file associated with the KEYS-ENTRIES.
+
+;; With prefix, rebuild the cache before offering candidates."
+;;   (interactive (list (citar-select-refs
+;;                       :rebuild-cache current-prefix-arg)))
+;;   (when (and citar-library-paths
+;;              (stringp citar-library-paths))
+;;     (message "Make sure 'citar-library-paths' is a list of paths"))
+;;   ;; (citar--library-files-action keys-entries 'open)
+;;   (let* ((key (car (car keys-entries)))
+;;          (pdf-file (car (bibtex-completion-find-pdf key))))
+;;       (if (file-exists-p pdf-file)
+;;           (funcall bibtex-completion-pdf-open-function pdf-file)
+;;         ;; (org-open-file pdf-file)
+;;         (message "No PDF found for %s" key-entries))))
+
+
+;;;###autoload
+(defun citar-open-bibtex-pdf ()
+  "Open pdf file associated with the CITE-KEY at point.
 
 With prefix, rebuild the cache before offering candidates."
-  (interactive (list (citar-select-refs
-                      :rebuild-cache current-prefix-arg)))
-  (when (and citar-library-paths
-             (stringp citar-library-paths))
-    (message "Make sure 'citar-library-paths' is a list of paths"))
-  ;; (citar--library-files-action keys-entries 'open)
-  (let* ((key (car (car keys-entries)))
+  (interactive)
+  (let* ((results (org-ref-get-bibtex-key-and-file))
+         (key (replace-regexp-in-string "@" "" (car results)))
          (pdf-file (car (bibtex-completion-find-pdf key))))
-      (if (file-exists-p pdf-file)
-          (funcall bibtex-completion-pdf-open-function pdf-file)
-        ;; (org-open-file pdf-file)
-        (message "No PDF found for %s" key-entries))))
+    ;; (pdf-file (car (bibtex-completion-find-pdf-in-field key))))
+    (if (file-exists-p pdf-file)
+        (org-open-file pdf-file)
+      (message "No PDF found for %s" key)))
+  )
