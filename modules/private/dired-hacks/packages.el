86cbaaff9a94545352712e05b0b524fe6e40d60e
Jonghyun Yun <jonghyun.yun@gmail.com>
2022-12-01 15:05:15 -0600

    diable ranger

--- modules/private/dired-hacks/packages.el
+++ modules/private/dired-hacks/packages.el
@@ -8,7 +8,7 @@
 (package! dired-subtree)
 (package! dired-collapse :disable t)
 (package! dired-rainbow :disable t)
-(package! dired-ranger)
+(package! dired-ranger :disable t)
 
 (package! dired-sidebar :pin "da77919081d9a4e73c2df63542353319381e4f89")
 
