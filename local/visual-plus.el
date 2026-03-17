b7e36bea7cd0b75953c2e7f1757d1a411ccda20a
Jonghyun Yun <jonghyun.yun@gmail.com>
2026-03-17 10:27:30 -0500

    fix(ui): Guard treemacs theme setup behind treemacs module
    Comment out doom-modeline customization and remove unicode font/NFD config

--- local/visual-plus.el
+++ local/visual-plus.el
@@ -107,10 +107,9 @@
   ;; :bind ("<f5>" . modus-themes-toggle)
   )
 
-;;; doom-theme
-(setq doom-themes-padded-modeline nil
-      doom-themes-treemacs-theme "doom-colors")
-(setq doom-themes-treemacs-enable-variable-pitch t
+(when (modulep! :ui treemacs)
+(setq doom-themes-treemacs-theme "doom-colors"
+      doom-themes-treemacs-enable-variable-pitch t
       doom-themes-treemacs-line-spacing 1
       doom-themes-treemacs-bitmap-indicator-width 2)
 (defface jyun/treemacs-face
@@ -118,106 +117,110 @@
   "A face to display file/diretory in treemacs."
   :group 'basic-faces)
 ;; (setq doom-themes-treemacs-variable-pitch-face 'jyun/treemacs-face)
+)
 
 ;;; doom-modeline
 ;; https://github.com/seagle0128/doom-modeline
-(with-eval-after-load 'doom-modeline
-  ;; (set-face-attribute 'mode-line nil :family "Noto Sans")
-  ;; (set-face-attribute 'mode-line-inactive nil :family "Noto Sans")
-  (set-face-attribute 'mode-line nil :inherit 'variable-pitch :height 0.95)
-  (set-face-attribute 'mode-line-inactive nil :inherit 'variable-pitch :height 0.95)
+;; (with-eval-after-load 'doom-modeline
+;;   ;; (set-face-attribute 'mode-line nil :family "Noto Sans")
+;;   ;; (set-face-attribute 'mode-line-inactive nil :family "Noto Sans")
+;;   (set-face-attribute 'mode-line nil :inherit 'variable-pitch :height 0.95)
+;;   (set-face-attribute 'mode-line-inactive nil :inherit 'variable-pitch :height 0.95)
 
-  ;; The right side of the modeline is cut off
-  (setq all-the-icons-scale-factor 1.1)
-  ;; (doom-modeline-def-modeline 'main
-  ;;                             '(bar matches buffer-info remote-host buffer-position parrot selection-info)
-  ;;                             '(misc-info minor-modes checker input-method buffer-encoding major-mode process vcs "  ")) ; <-- added padding here
+;;   ;; The right side of the modeline is cut off
+;;   (setq all-the-icons-scale-factor 1.1)
+;;   ;; (doom-modeline-def-modeline 'main
+;;   ;;                             '(bar matches buffer-info remote-host buffer-position parrot selection-info)
+;;   ;;                             '(misc-info minor-modes checker input-method buffer-encoding major-mode process vcs "  ")) ; <-- added padding here
 
-  ;; How tall the mode-line should be. It's only respected in GUI.
-  ;; If the actual char height is larger, it respects the actual height.
-  (setq doom-modeline-height 18)
+;;   ;; How tall the mode-line should be. It's only respected in GUI.
+;;   ;; If the actual char height is larger, it respects the actual height.
+;;   (setq doom-modeline-height 18)
 
-  ;; to see letters instead of the colored circles
-  (setq doom-modeline-modal-icon nil)
+;;   ;; to see letters instead of the colored circles
+;;   (setq doom-modeline-modal-icon nil)
 
-  ;; https://github.com/seagle0128/doom-modeline/issues/187#issuecomment-507201556
-  ;; (defun my-doom-modeline--font-height ()
-  ;;   "Calculate the actual char height of the mode-line."
-  ;;   (+ (frame-char-height) 2))
-  ;; (advice-add #'doom-modeline--font-height :override #'my-doom-modeline--font-height)
+;;   ;; https://github.com/seagle0128/doom-modeline/issues/187#issuecomment-507201556
+;;   ;; (defun my-doom-modeline--font-height ()
+;;   ;;   "Calculate the actual char height of the mode-line."
+;;   ;;   (+ (frame-char-height) 2))
+;;   ;; (advice-add #'doom-modeline--font-height :override #'my-doom-modeline--font-height)
 
-  ;; How wide the mode-line bar should be. It's only respected in GUI.
-  (setq doom-modeline-bar-width 2)
+;;   ;; How wide the mode-line bar should be. It's only respected in GUI.
+;;   (setq doom-modeline-bar-width 2)
 
-  ;; The limit of the window width.
-  ;; If `window-width' is smaller than the limit, some information won't be displayed.
-  (setq doom-modeline-window-width-limit fill-column)
+;;   ;; The limit of the window width.
+;;   ;; If `window-width' is smaller than the limit, some information won't be displayed.
+;;   (setq doom-modeline-window-width-limit fill-column)
 
-  ;; How to detect the project root.
-  ;; The default priority of detection is `ffip' > `projectile' > `project'.
-  ;; nil means to use `default-directory'.
-  ;; The project management packages have some issues on detecting project root.
-  ;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
-  ;; to hanle sub-projects.
-  ;; You can specify one if you encounter the issue.
-  (setq doom-modeline-project-detection 'ffip)
+;;   ;; How to detect the project root.
+;;   ;; The default priority of detection is `ffip' > `projectile' > `project'.
+;;   ;; nil means to use `default-directory'.
+;;   ;; The project management packages have some issues on detecting project root.
+;;   ;; e.g. `projectile' doesn't handle symlink folders well, while `project' is unable
+;;   ;; to hanle sub-projects.
+;;   ;; You can specify one if you encounter the issue.
+;;   (setq doom-modeline-project-detection 'ffip)
 
-  ;; Determines the style used by `doom-modeline-buffer-file-name'.
-  ;;
-  ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
-  ;;   auto => emacs/lisp/comint.el (in a project) or comint.el
-  ;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
-  ;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
-  ;;   truncate-with-project => emacs/l/comint.el
-  ;;   truncate-except-project => ~/P/F/emacs/l/comint.el
-  ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
-  ;;   truncate-all => ~/P/F/e/l/comint.el
-  ;;   relative-from-project => emacs/lisp/comint.el
-  ;;   relative-to-project => lisp/comint.el
-  ;;   file-name => comint.el
-  ;;   buffer-name => comint.el<2> (uniquify buffer name)
-  ;;
-  ;; If you are experiencing the laggy issue, especially while editing remote files
-  ;; with tramp, please try `file-name' style.
-  ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
-  (setq doom-modeline-buffer-file-name-style 'auto)
+;;   ;; Determines the style used by `doom-modeline-buffer-file-name'.
+;;   ;;
+;;   ;; Given ~/Projects/FOSS/emacs/lisp/comint.el
+;;   ;;   auto => emacs/lisp/comint.el (in a project) or comint.el
+;;   ;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
+;;   ;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
+;;   ;;   truncate-with-project => emacs/l/comint.el
+;;   ;;   truncate-except-project => ~/P/F/emacs/l/comint.el
+;;   ;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
+;;   ;;   truncate-all => ~/P/F/e/l/comint.el
+;;   ;;   relative-from-project => emacs/lisp/comint.el
+;;   ;;   relative-to-project => lisp/comint.el
+;;   ;;   file-name => comint.el
+;;   ;;   buffer-name => comint.el<2> (uniquify buffer name)
+;;   ;;
+;;   ;; If you are experiencing the laggy issue, especially while editing remote files
+;;   ;; with tramp, please try `file-name' style.
+;;   ;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+;;   (setq doom-modeline-buffer-file-name-style 'auto)
 
-  ;; Whether display icons in the mode-line. Respects `all-the-icons-color-icons'.
-  ;; While using the server mode in GUI, should set the value explicitly.
-  (setq doom-modeline-icon nil)
+;;   ;; Whether display icons in the mode-line. Respects `all-the-icons-color-icons'.
+;;   ;; While using the server mode in GUI, should set the value explicitly.
+;;   (setq doom-modeline-icon nil)
 
-  ;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
-  (setq doom-modeline-buffer-state-icon nil)
+;;   ;; Whether display the icon for the buffer state. It respects `doom-modeline-icon'.
+;;   (setq doom-modeline-buffer-state-icon nil)
 
-  ;; Whether display the buffer encoding.
-  (setq doom-modeline-buffer-encoding nil)
+;;   ;; Whether display the buffer encoding.
+;;   (setq doom-modeline-buffer-encoding nil)
 
-  ;; Whether display the workspace name. Non-nil to display in the mode-line.
-  (setq doom-modeline-workspace-name nil)
+;;   ;; Whether display the workspace name. Non-nil to display in the mode-line.
+;;   (setq doom-modeline-workspace-name nil)
 
-  ;; Whether display perspective name. Non-nil to display in mode-line.
-  (setq doom-modeline-persp-name t)
+;;   ;; Whether display perspective name. Non-nil to display in mode-line.
+;;   (setq doom-modeline-persp-name t)
 
-  ;; If non nil the perspective name is displayed alongside a folder icon.
-  (setq doom-modeline-persp-icon nil)
+;;   ;; If non nil the perspective name is displayed alongside a folder icon.
+;;   (setq doom-modeline-persp-icon nil)
 
-  ;; Whether display the `lsp' state. Non-nil to display in the mode-line.
-  (setq doom-modeline-lsp nil)
+;;   ;; Whether display the `lsp' state. Non-nil to display in the mode-line.
+;;   (setq doom-modeline-lsp nil)
 
-  ;; Whether display mu4e notifications. It requires `mu4e-alert' package.
-  (setq doom-modeline-mu4e nil)
-  ;; also enable the start of mu4e-alert
-  ;; (mu4e-alert-enable-mode-line-display)
+;;   ;; Whether display mu4e notifications. It requires `mu4e-alert' package.
+;;   (setq doom-modeline-mu4e nil)
+;;   ;; also enable the start of mu4e-alert
+;;   ;; (mu4e-alert-enable-mode-line-display)
 
-  ;; Whether display the gnus notifications.
-  (setq doom-modeline-gnus nil)
+;;   ;; Whether display the gnus notifications.
+;;   (setq doom-modeline-gnus nil)
 
-  ;; Whether display the IRC notifications. It requires `circe' or `erc' package.
-  (setq doom-modeline-irc nil)
+;;   ;; Whether display the IRC notifications. It requires `circe' or `erc' package.
+;;   (setq doom-modeline-irc nil)
 
-  ;; Whether display the environment version.
-  (setq doom-modeline-env-version nil)
-  )
+;;   ;; Whether display the environment version.
+;;   (setq doom-modeline-env-version nil)
+
+;;   ;; doom-theme
+;;   (setq doom-themes-padded-modeline nil)
+;;   )
 
 ;;; evil-cursor
 ;; (defun jyun/evil-state-cursors ()
@@ -282,25 +285,3 @@
 ;;   (evil-set-cursor-color (get 'cursor 'evil-visual-color)))
 ;; (defun +evil-motion-cursor-fn ()
 ;;   (evil-set-cursor-color (get 'cursor 'evil-motion-color)))
-
-;;; unicode
-(after! unicode-fonts
-  ;; fix Hangul fonts for Jamo
-  (dolist (unicode-block '("Hangul Compatibility Jamo"
-                           "Hangul Jamo"
-                           "Hangul Jamo Extended-A"
-                           "Hangul Jamo Extended-B"
-                           "Hangul Syllables"))
-    (push "Sarasa Mono K" (cadr (assoc unicode-block unicode-fonts-block-font-mapping))))
-  ;; (push "Noto Sans CJK KR" (cadr (assoc unicode-block unicode-fonts-block-font-mapping))))
-  ;; (push "Source Han Sans K" (cadr (assoc unicode-block unicode-fonts-block-font-mapping))))
-  ;; (push "Source Han Serif K" (cadr (assoc unicode-block unicode-fonts-block-font-mapping))))
-  )
-
-;; Dropbox sync changes hangul encoding to NFD, which results in 한글 자소분리 in dired and other modes
-;; https://tt.kollhong.com/79
-;; https://nullprogram.com/blog/2014/06/13/
-(when IS-MAC
-  (use-package! ucs-normalize
-    :config
-    (set-file-name-coding-system 'utf-8-hfs)))
